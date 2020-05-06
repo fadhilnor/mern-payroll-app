a4910795c6734b2dee8424bff56ab0846be2cccf
dale7117 <45253407+dale7117@users.noreply.github.com>
2020-05-06 15:48:20 +0800

    Add serve static file in production

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ app.use(passport.session());
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
 }
 
 // HTTP request logger
