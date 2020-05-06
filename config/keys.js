653ffd66fe60c71aa8981ce6713db3b2261e3ead
dale7117 <45253407+dale7117@users.noreply.github.com>
2020-05-06 16:59:12 +0800

    Include config keys to gitignore

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-dbPassword = 'mongodb+srv://admin:admin123@cluster0-yku3y.mongodb.net/mern-payroll?retryWrites=true&w=majority';
-secretOrKey = 'https://tinyurl.com/y3yhwvjz';
-
-module.exports = {
-  mongoURI: dbPassword,
-  secretOrKey,
-};
