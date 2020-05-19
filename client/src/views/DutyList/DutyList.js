7cf6fce3c68f0cc8b2d685c6a7ffadcb736f8aa5
dale7117 <45253407+dale7117@users.noreply.github.com>
2020-05-19 16:03:22 +0800

    Add duty list

---
+++ client/src/views/DutyList/DutyList.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/styles';
+
+import { DutyTable } from './components';
+import { getDuties } from '../../services/dutyServices';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(3),
+  },
+  content: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const DutyList = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const { duty } = useSelector((state) => state.duties);
+
+  const loadDuties = useCallback(async () => {
+    dispatch(getDuties());
+  }, [dispatch]);
+
+  useEffect(() => {
+    loadDuties();
+  }, [loadDuties]);
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.content}>
+        <DutyTable duties={duty} />
+      </div>
+    </div>
+  );
+};
+
+export default DutyList;
--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,3 +1,4 @@
 export { default as Dashboard } from './Dashboard';
 export { default as Settings } from './Settings';
 export { default as PositionList } from './PositionList';
+export { default as DutyList } from './DutyList';
