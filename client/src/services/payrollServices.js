9b12ea6710bfe5c5e68b286065d662ed0df9568d
dale7117 <45253407+dale7117@users.noreply.github.com>
2020-05-31 13:59:44 +0800

    Add new payroll services

--- client/src/services/payrollServices.js
+++ client/src/services/payrollServices.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { GET_PAYROLLS } from './types';
+import { setSnackbarMessageSuccess, setSnackbarMessageError } from './snackbarServices';
+import { setUserLoading } from './authServices';
 
 // Get all payrolls
 export const getPayrolls = () => (dispatch) => {
@@ -10,6 +12,31 @@ export const getPayrolls = () => (dispatch) => {
     })
     .catch((err) => {
       console.log('err', err);
+    });
+};
+
+// Add new payroll
+export const addPayroll = (payroll) => async (dispatch) => {
+  // Toggle on loading animation
+  dispatch(setUserLoading(true));
+
+  return await axios
+    .post('/payrolls/add', payroll)
+    .then((res) => {
+      // Toggle off loading animation
+      dispatch(setUserLoading(false));
+
+      // Toggle on notification
+      dispatch(setSnackbarMessageSuccess(res.data.msg));
+      return Promise.resolve(res.data.payrolls);
+    })
+    .catch((err) => {
+      // Toggle off loading animation
+      dispatch(setUserLoading(false));
+
+      // Toggle on notification
+      dispatch(setSnackbarMessageError(err.response.data));
+      return Promise.reject();
     });
 };
 
