3b6e6469079ddf3dbcc8972c9006d2d98eaf80ee
dale7117 <45253407+dale7117@users.noreply.github.com>
2020-06-01 12:06:24 +0800

    Add payroll employee model

---
+++ models/PayrollEmployee.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const PayrollEmployeeSchema = new mongoose.Schema(
+  {
+    payId: {
+      type: Number,
+      required: true,
+    },
+    empId: {
+      type: String,
+      required: true,
+    },
+    day: {
+      type: Number,
+      required: true,
+    },
+    duty: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const PayrollEmployee = mongoose.model('PayrollEmployee', PayrollEmployeeSchema);
+
+module.exports = PayrollEmployee;
