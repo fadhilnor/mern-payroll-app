d6cd32c55274a54be856cabfc1d13da3aaccad2e
dale7117 <45253407+dale7117@users.noreply.github.com>
2020-06-04 11:54:35 +0800

    Edit dashboard view to add Monthy Amount chart

--- client/src/views/Dashboard/Dashboard.js
+++ client/src/views/Dashboard/Dashboard.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/styles';
-import { Typography as MuiTypography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+
+import { MonthlyAmount } from './components';
+
+import { getAllPayrollEmployees } from '../../services/dashboardServices';
+import { getPayrolls } from '../../services/payrollServices';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,10 +16,50 @@ const useStyles = makeStyles((theme) => ({
 
 const Dashboard = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const { payroll } = useSelector((state) => state.payrolls);
+  const { employePayrolls } = useSelector((state) => state.dashboard);
+
+  const loadEmployeePayrolls = useCallback(async () => {
+    dispatch(getAllPayrollEmployees());
+  }, [dispatch]);
+
+  const loadPayrolls = useCallback(async () => {
+    dispatch(getPayrolls());
+  }, [dispatch]);
+
+  useEffect(() => {
+    loadEmployeePayrolls();
+    loadPayrolls();
+  }, [loadPayrolls, loadEmployeePayrolls]);
 
   return (
     <div className={classes.root}>
-      <MuiTypography variant="h3">Welcome to Dashboard!</MuiTypography>
+      <Grid container spacing={4}>
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
+          {/* <MonthlyAmount payroll={payroll} /> */}
+        </Grid>
+        <Grid item lg={3} sm={6} xl={3} xs={12}></Grid>
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
+          {/* <TasksProgress /> */}
+        </Grid>
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
+          {/* <TotalProfit /> */}
+        </Grid>
+        <Grid item lg={8} md={12} xl={9} xs={12}>
+          {/* <LatestSales /> */}
+          <MonthlyAmount employePayrolls={employePayrolls} payroll={payroll} />
+        </Grid>
+        <Grid item lg={4} md={6} xl={3} xs={12}>
+          {/* <UsersByDevice /> */}
+        </Grid>
+        <Grid item lg={4} md={6} xl={3} xs={12}>
+          {/* <LatestProducts /> */}
+        </Grid>
+        <Grid item lg={8} md={12} xl={9} xs={12}>
+          {/* <LatestOrders /> */}
+        </Grid>
+      </Grid>
     </div>
   );
 };
